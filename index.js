9e280128a936b23c0b21fb18bbf5ff89382c8f29
Samuel Wilson <samwil56@gmail.com>
2021-02-09 21:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,18 @@ function appendKitten(name) {
   var newkit = [...kittens,name]
   return newkit
 }
+
+function prependKitten(name) {
+  var newkit = [name,...kittens]
+  return newkit
+}
+
+function removeLastKitten() {
+  var newkit = kittens.splice(0,kittens.length-2)
+  return newkit
+}
+
+function removeFirstKitten() {
+  var newkit = kittens.splice(1,kittens.length)
+  return newkit
+}
